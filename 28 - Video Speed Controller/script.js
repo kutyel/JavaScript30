bee3538359557ca378978f9e8067a6c8ae5c1fa7
kutyel <flaviocorpa@gmail.com>
2017-05-24 11:21:44 +0200

    Solve 28 - Video Speed Controller

---
+++ 28 - Video Speed Controller/script.js
@@ -0,0 +1,14 @@
+const speed = document.querySelector('.speed')
+const bar = speed.querySelector('.speed-bar')
+const video = document.querySelector('.flex')
+
+speed.addEventListener('mousemove', function ({ pageY }) {
+  const y = pageY - this.offsetTop
+  const percent = y / this.offsetHeight
+  const [min, max] = [0.4, 4]
+  const height = Math.round(percent * 100) + '%'
+  const playbackRate = percent * (max - min) + min
+  bar.style.height = height
+  bar.textContent = playbackRate.toFixed(2) + '×'
+  video.playbackRate = playbackRate
+})
